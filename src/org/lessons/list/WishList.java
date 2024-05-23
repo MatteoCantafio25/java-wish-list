9476f94e99ce7c38844ec2fd6e025a99178d4aa4
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-23 17:04:14 +0200

    Bonus completed

--- src/org/lessons/list/WishList.java
+++ src/org/lessons/list/WishList.java
@@ -1,11 +1,11 @@
 package org.lessons.list;
-
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class WishList {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // Creo le variabili
         String userChoice;
@@ -17,6 +17,24 @@ public class WishList {
 
         // Creo la lista
         ArrayList<String> list = new ArrayList<>();
+
+        // Creo un oggetto file che rappresenta il file dove verranno salvati i regali
+        File listText = new File("./resources/data.txt");
+
+        // Creo uno scanner per leggere i regali dal file
+        Scanner listReader = new Scanner(listText);
+
+        // Ciclo per leggere le righe del file
+        while(listReader.hasNextLine()){
+
+            // Leggo la riga successiva
+            String oldLine = listReader.nextLine();
+
+            // Aggiungo la riga letta alla lista
+            list.add(oldLine);
+        }
+        // Chiudo lo scanner
+        listReader.close();
 
         // Ciclo finchè l'utente vuole andare avanti
         while (goNext){
@@ -36,17 +54,33 @@ public class WishList {
                 // Mostro a video la lunghezza della lista
                 System.out.println("Presents: " + list.size());
 
+                // Ordino la lista in ordine alfabetico
+                Collections.sort(list);
+
+                // Creo un oggetto FileWriter per scrivere nel file
+                FileWriter listWriter = new FileWriter(listText);
+
+                // Ciclo su ogni singolo elemento della lista
+                for (String element : list){
+
+                    // Scrivo l'elemento della lista e mando a capo
+                    listWriter.write(element + "\n");
+                }
+                // Chiudo il listWriter
+                listWriter.close();
+
             // Altrimenti
             }else {
                 // Esco dal ciclo
                 goNext = false;
 
-                // Ordino la lista in ordine alfabetico
-                Collections.sort(list);
+                // Se la lista non è vuota
+                if (!list.isEmpty()){
 
-                // Mostro a video il resoconto della lista
-                System.out.print(list);
-                System.out.print(" Santa is coming....");
+                    // Mostro a video il resoconto della lista
+                    System.out.print(list);
+                    System.out.print(" Santa is coming....");
+                }
             }
         }
 
